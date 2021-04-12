3f3785ffb4b39f5fbc1c9655192ef06d4448b157
olga <olga.i.rubleva@gmail.com>
2021-04-11 18:07:24 -0700

    Pushing all files at once

---
+++ 15-cocktails/setup/src/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+  return (
+      <section className="section about-section">
+          <h1 className="section-title">about us</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae, sunt, tenetur! Eveniet impedit iusto magnam nobis officiis similique vero, voluptatum! Animi error facere labore laboriosam molestiae qui ratione recusandae reiciendis.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae, sunt, tenetur! Eveniet impedit iusto magnam nobis officiis similique vero, voluptatum! Animi error facere labore laboriosam molestiae qui ratione recusandae reiciendis.</p>
+      </section>
+  )
+}
+
+export default About
